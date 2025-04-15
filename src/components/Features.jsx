0f0a846a8aebecb5ff474a0760409e74769bf098
ssr <shubhamsaha.ray@drishinfo.com>
2025-04-15 09:03:09 +0530

    fixing css

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -15,8 +15,8 @@ const BentoTilt = ({ children, className = "" }) => {
       const relativeX = (event.clientX - left) / width;
       const relativeY = (event.clientY - top) / height;
   
-      const tiltX = (relativeY - 0.5) * 5;
-      const tiltY = (relativeX - 0.5) * -5;
+      const tiltX = (relativeY - 0.5) * 20;
+      const tiltY = (relativeX - 0.5) * -20;
   
       const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(.95, .95, .95)`;
       setTransformStyle(newTransform);
@@ -65,12 +65,15 @@ const Features = () => {
     return (
         <section className="bg-black pb-52">
             <div className="container mx-auto px-3 md:px-10">
-                <div className="px-5 py-32">
-                    <p className="font-circular-web text-lg text-blue-50">Into the Metagame Layer</p>
-                </div>
+                <div className="px-1 py-24">
+                <p className="font-circular-web text-lg text-blue-50">
+                    Into the Metagame Layer
+                </p>
+
                 <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
                     Immerse yourself in a rich and ever-expanding universe where a vibrant array of products converge into an interconnected overlay experience on your world.
                 </p>
+                </div>
 
                 <BentoTilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
                     <BentoCard 
