4203aa1889dccfa705950644771fe2b31f11cd71
Arsh Singhal <arsh.singhal@drishinfo.com>
2025-04-15 14:03:54 +0530

    Redesigning application

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
 import { useGSAP } from "@gsap/react";
@@ -8,54 +8,20 @@ import { ScrollTrigger } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger)
 
 const Hero = () => {
-    const [currentIndex, setCurrentIndex] = useState(1);
-    const [hasClicked, setHasClicked] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
-    const [loadedVideos, setLoadedVideos] = useState(0);
+    const [loadedVideo, setLoadedVideo] = useState(false);
 
-    const totalVideos = 4;
-    const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
-    const nextVideoRef = useRef(null);
-
-    const handleMiniVdClick = () =>{
-        setHasClicked(true);
-        setCurrentIndex(upcomingVideoIndex)
-    }
-    
     useEffect(() =>{
-        if(loadedVideos === totalVideos-1){
+        if(loadedVideo){
             setIsLoading(false);
         }
-    }, [loadedVideos])
+    }, [loadedVideo])
 
     const handleVideoLoaded = () => {
-        setLoadedVideos((prev) => prev+1);
+        setLoadedVideo(true);
     }
 
-
-    useGSAP(() =>{
-        if(hasClicked){
-            gsap.set("#next-video", {visibility: 'visible'});
-
-            gsap.to("#next-video",{
-                transformOrigin: "center center",
-                scale: 1,
-                width: "100%",
-                height: "100%",
-                duration: 1,
-                ease: "power1.inOut", 
-                onStart: () => nextVideoRef.current.play(),
-            });;
-
-            gsap.from("#current-video", {
-                transformOrigin: "center center",
-                scale: 0,
-                duration: 1.5,
-                ease: "power1.inOut",
-            });
-        }
-    }, {dependencies:[currentIndex], revertOnUpdate: true});
-
+    // Animate the Hero Section (creating a Polygon during Scroll event.)
     useGSAP(() =>{
         gsap.set('#video-frame', {
             clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
@@ -74,10 +40,12 @@ const Hero = () => {
         })
     })
 
-    const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
     return(
+        // HERO SECTION
         <div className="relative h-dvh w-screen overflow-x-hidden">
+
+            {/* Loading Animation */}
             {isLoading && (
                 <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
                 <div className="three-body">
@@ -89,31 +57,10 @@ const Hero = () => {
             )}
             
             <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
+                {/* Video Section */}
                 <div>
-                    <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-                        <div onClick={handleMiniVdClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
-                            <video 
-                                ref={nextVideoRef} 
-                                src = {getVideoSrc(upcomingVideoIndex)} 
-                                loop 
-                                muted 
-                                id="current-video"
-                                className="size-64 origin-center scale-150 object-cover object-center"
-                                onLoadedData = {handleVideoLoaded}
-                            />
-                        </div>
-                    </div>
-                    <video
-                        ref={nextVideoRef}
-                        src={getVideoSrc(currentIndex)}
-                        loop
-                        muted
-                        id="next-video"
-                        className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
-                        onLoadedData={handleVideoLoaded}
-                    />
                     <video 
-                        src={getVideoSrc(currentIndex === totalVideos-1 ? 1 : currentIndex)}
+                        src="videos/hero-1.mp4"
                         autoPlay
                         loop
                         muted
@@ -121,21 +68,27 @@ const Hero = () => {
                         onLoadedData={handleVideoLoaded}
                     />
                 </div>
+
+                {/* Hero Footer during Video play*/}
                 <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
                     G<b>a</b>ming
                 </h1>
+
+                {/*Hero Heading Section */}
                 <div className="absolute left-0 top-0 z-40 size-full">
                     <div className="mt-24 px-5 sm:px-10">
-                        <h1 className="special-font hero-heading text-blue-100"> redefi<b>n</b>e</h1>
-                        <p className="mb-5 max-w-64 font-robert-regular text-blue-100"> Enter the Metagame layer<br /> Unleash the Play Economy</p>
+                        <h1 className="special-font hero-heading text-blue-100"> disc<b>o</b>ver</h1>
+                        <p className="mb-5 max-w-64 font-general text-blue-100 uppercase"> Enter the Metagame layer<br/> Unleash the Play Economy</p>
                     
-                    <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow />} containerClass="!bg-yellow-300 flex-center gap-1"/>
+                    <Button id="watch-trailer" title="Ready" leftIcon={<TiLocationArrow />} containerClass="!bg-yellow-300 flex-center gap-1"/>
                     </div>
                 </div>
             </div>
+            
+            {/*Change the Text of Hero Footer during Animation*/}
             <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
                     G<b>a</b>ming
-                </h1>
+            </h1>
         </div>
     )
 }
